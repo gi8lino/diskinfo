ef02b6cd1c242661004ea2185344f15959550c21
gi8 <giottolino@gmail.com>
2019-10-14 16:49:01 +0200

    add sorted

--- diskinfo.sh
+++ diskinfo.sh
@@ -101,5 +101,6 @@ while IFS=' ', read -a input; do
     if [[ ! " ${EXCLUDES[@]} " =~ " ${filesystem} " ]];then
         printf "%-22s%8s%8s%8s%4s%-${BARLENGTH}s%3s%4s%-s\n" ${mounted} ${size} ${used} ${avail} "" "$(ShowUsage ${use::-1} ${BARLENGTH}) " ${use} "" ${filesystem}
     fi
-done <<< "$(df -h)"
+done <<< "$(df -h)" |
+sort -n -k1
 exit 0
