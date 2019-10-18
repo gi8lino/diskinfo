ec32655f7eebd84329634e41bd67ecc372272cb5
gi8 <giottolino@gmail.com>
2019-10-18 18:35:27 +0200

    update

--- diskinfo.sh
+++ diskinfo.sh
@@ -172,7 +172,7 @@ while IFS=' ', read -a input; do
 
     # check if filesystem is in excluded list
     if [[ ! " ${EXCLUDES[@]} " =~ " ${filesystem} " ]];then
-        printf "%-22s%8s%8s%8s%4s%-${BARLENGTH}s%3s%4s%-s\n" ${mounted} ${size} ${used} ${avail} "" "$(ShowUsage ${use::-1} ${BARLENGTH}) " ${use} "" ${filesystem}
+        printf "%-22s%8s%8s%8s%4s%-${BARLENGTH} %4s%-s\n" ${mounted} ${size} ${used} ${avail} "$(ShowUsage ${use::-1} ${BARLENGTH}) " ${use} ${filesystem}
     fi
 
 done <<< "$(df -h | tail -n +2)" | sort -k$SORTED_BY $REVERSE
