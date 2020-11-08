cc1b72553cbec17d93a5ad3ee4aa58ec39c0fe79
gi8 <giottolino@gmail.com>
2020-11-09 00:37:14 +0100

    replace tabs with spaces

--- diskinfo.sh
+++ diskinfo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-VERSION="v1.0.8"
+VERSION="v1.0.9"
 
 ShowUsage() {
     local _percent=$1
     local _barlength=$2
-  
+
     ((_rounded = (${_percent}+2)/5, _rounded *= 5))  # round to the next five percent
     let _used=(_barlength*_rounded/100)  # used in relation to bar length
     let _free=(_barlength-_used)  # rest
-       
+
     # progressbar string lengths
     local _fill=$(printf "%${_used}s")
     local _empty=$(printf "%${_free}s")
@@ -25,7 +25,7 @@ Usage: diskinfo.sh [-e|--exclude-types \"TYPE ...\"]
                    [-r|--reverse]
                    | [-h|--help]
                    | [-v|--version]
-                                
+
 Show diskinfo (df -h) with a progressbar for disk usage.
 The progressbar will round up/down the progress to the next 5 percent.
 The disk usage in percent next to the progressbar will not be rounded.
@@ -65,39 +65,39 @@ unset IFS
 while [ $# -gt 0 ]; do
     key="$1"
     case $key in
-	    -e|--exclude-types)
-	    EXCLUDES="$2"
-	    shift  # pass argument
-	    shift  # pass value
-	    ;;
-	    -b|--bar-length)
-	    BARLENGTH="$2"
-	    shift  # pass argument
-	    shift  # pass value
-	    ;;
-	    -s|--sort)
-	    SORTKEY="$2"
-	    shift  # pass argument
-	    shift  # pass value
-	    ;;
-	    -r|--reverse)
-	    REVERSE="-r"
-	    shift  # pass argument
-	    ;;
-	    -v|--version)
-	    printf "$(basename $BASH_SOURCE) version: %s\n" "${VERSION}"
-	    exit 0
-	    ;;
-	    -h|--help)
-	    ShowHelp
-	    ;;
-	    *)  # unknown option
-	    printf "%s\n" \
-	       "$(basename $BASH_SOURCE): invalid option -- '$1'" \
-	       "Try '$(basename $BASH_SOURCE) --help' for more information."
-    exit 1
-	    ;;
-    esac  # end case
+        -e|--exclude-types)
+        EXCLUDES="$2"
+        shift
+        shift
+        ;;
+        -b|--bar-length)
+        BARLENGTH="$2"
+        shift
+        shift
+        ;;
+        -s|--sort)
+        SORTKEY="$2"
+        shift
+        shift
+        ;;
+        -r|--reverse)
+        REVERSE="-r"
+        shift
+        ;;
+        -v|--version)
+        printf "$(basename $BASH_SOURCE) version: %s\n" "${VERSION}"
+        exit 0
+        ;;
+        -h|--help)
+        ShowHelp
+        ;;
+        *)  # unknown option
+        printf "%s\n" \
+           "$(basename $BASH_SOURCE): invalid option -- '$1'" \
+           "Try '$(basename $BASH_SOURCE) --help' for more information."
+        exit 1
+        ;;
+    esac
 done
 
 [ -z "${BARLENGTH##*[!0-9]*}" ] && BARLENGTH=20  # if barlength value is not set or not a number, set barlength to 20
@@ -113,6 +113,7 @@ done
 
 declare -a diskinfo
 MOUNTED_LEN=15
+
 # collect disk usage
 while IFS=' ', read -ra input; do
     filesystem="${input[0]}"
@@ -163,24 +164,24 @@ if [ -n "${SORTKEY}" ]; then
         SORTED_BY="2 -h"
         sort_size_correction=3
         sort_used_correction=-1
-	    SIZE_SORT="$SORT_DIRECTION"
+        SIZE_SORT="$SORT_DIRECTION"
         ;;
         used|ud)
         SORTED_BY="3 -h"
         sort_used_correction=3
         sort_free_correction=-1
-	    USED_SORT="$SORT_DIRECTION"
+        USED_SORT="$SORT_DIRECTION"
         ;;
         free|f)
         SORTED_BY="4 -h"
         sort_free_correction=3
         sort_usage_correction=-1
-	    FREE_SORT="$SORT_DIRECTION"
+        FREE_SORT="$SORT_DIRECTION"
         ;;
         usage|ug)
         SORTED_BY="6 -h"
         sort_usage_correction=3
-	    sort_filesystem_correction=2
+        sort_filesystem_correction=2
         USAGE_SORT="$SORT_DIRECTION"
         ;;
         filesystem|fs)
